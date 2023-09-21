099b515b8cfc18bb780da34a6f400af33bed06d7
Zequi20 <ezequiel234242@gmail.com>
2023-09-21 10:13:30 -0400

    fix inForm time picker

--- lib/widgets/custom/fields/custom_time_number_picker.dart
+++ lib/widgets/custom/fields/custom_time_number_picker.dart
@@ -34,6 +34,21 @@ class _CustomTimeNumberPickerState extends State<CustomTimeNumberPicker> {
     super.initState();
     pickerValue = widget.initialValue;
     widget.textController.text = getTimeValue(pickerValue);
+    foco.addListener(() {
+      String value = widget.textController.text;
+      if (!foco.hasFocus) {
+        if (value.isEmpty) {
+          widget.textController.text = '00';
+        } else {
+          if (int.parse(value) > widget.max) {
+            widget.textController.text = '00';
+          }
+          if (value.length < 2) {
+            widget.textController.text = '0$value';
+          }
+        }
+      }
+    });
   }
 
   String getTimeValue(int value) {
@@ -46,97 +61,101 @@ class _CustomTimeNumberPickerState extends State<CustomTimeNumberPicker> {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 50,
-      child: Row(
-        children: [
-          SizedBox(
-            width: 45,
-            child: TextFormField(
-              textAlign: TextAlign.center,
-              onChanged: (value) {
-                int valor = int.parse(value);
-                if (valor > widget.max) {
-                  widget.textController.text = widget.max.toString();
-                }
-                if (valor < 10) {
-                  widget.textController.text = '0$valor';
-                }
-                setState(() {});
-              },
-              inputFormatters: [
-                FilteringTextInputFormatter.allow(RegExp(r'[0-9]')),
-              ],
-              focusNode: foco,
-              onFieldSubmitted: (value) {
-                if (int.parse(value) > widget.max) {
-                  setState(() {
-                    widget.textController.text = widget.max.toString();
-                  });
-                }
-                FocusScope.of(context).requestFocus(foco);
-              },
-              controller: widget.textController,
-              decoration: InputDecoration(
-                  border: InputBorder.none,
-                  enabledBorder: InputBorder.none,
-                  focusedBorder: const OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.orange, width: 7)),
-                  hintText: widget.hint,
-                  filled: true,
-                  fillColor: fillColor),
-            ),
-          ),
-          FittedBox(
-            child: Container(
-              width: 30,
-              decoration: const BoxDecoration(
-                  color: Colors.transparent,
-                  borderRadius: BorderRadius.all(Radius.circular(5))),
-              child: Column(
-                children: [
-                  ExcludeFocus(
-                    excluding: true,
-                    child: IconButton(
-                        focusColor: Colors.orange,
-                        onPressed: () {
-                          if (pickerValue < widget.max) {
-                            setState(() {
-                              pickerValue++;
-                              widget.textController.text =
-                                  getTimeValue(pickerValue);
-                            });
-                          }
-                        },
-                        icon: const Icon(
-                          Icons.arrow_drop_up,
-                          size: 15,
-                        )),
-                  ),
-                  ExcludeFocus(
-                    excluding: true,
-                    child: IconButton(
-                        focusColor: Colors.orange,
-                        onPressed: () {
-                          if (pickerValue > 0) {
-                            setState(() {
-                              pickerValue--;
-                              widget.textController.text =
-                                  getTimeValue(pickerValue);
-                            });
-                          }
-                        },
-                        icon: const Icon(
-                          Icons.arrow_drop_down,
-                          size: 15,
-                        )),
-                  )
+    return RawKeyboardListener(
+      focusNode: FocusNode(),
+      onKey: (RawKeyEvent event) {
+        if (event.logicalKey == LogicalKeyboardKey.arrowUp) {
+          aumentar();
+        } else if (event.logicalKey == LogicalKeyboardKey.arrowDown) {
+          disminuir();
+        }
+      },
+      child: SizedBox(
+        height: 50,
+        child: Row(
+          children: [
+            SizedBox(
+              width: 45,
+              child: TextFormField(
+                textAlign: TextAlign.center,
+                inputFormatters: [
+                  FilteringTextInputFormatter.digitsOnly,
+                  FilteringTextInputFormatter.allow(
+                      RegExp(r'^[0-9]{0,2}$')), // Limitar a 2 dígitos
+                  FilteringTextInputFormatter.digitsOnly,
                 ],
+                focusNode: foco,
+                onFieldSubmitted: (value) {
+                  FocusScope.of(context).requestFocus(foco);
+                },
+                controller: widget.textController,
+                decoration: InputDecoration(
+                    border: InputBorder.none,
+                    enabledBorder: InputBorder.none,
+                    focusedBorder: const OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.orange, width: 7)),
+                    hintText: widget.hint,
+                    filled: true,
+                    fillColor: fillColor),
               ),
             ),
-          )
-        ],
+            FittedBox(
+              child: Container(
+                width: 30,
+                decoration: const BoxDecoration(
+                    color: Colors.transparent,
+                    borderRadius: BorderRadius.all(Radius.circular(5))),
+                child: Column(
+                  children: [
+                    ExcludeFocus(
+                      excluding: true,
+                      child: IconButton(
+                          focusColor: Colors.orange,
+                          onPressed: () {
+                            aumentar();
+                          },
+                          icon: const Icon(
+                            Icons.arrow_drop_up,
+                            size: 15,
+                          )),
+                    ),
+                    ExcludeFocus(
+                      excluding: true,
+                      child: IconButton(
+                          focusColor: Colors.orange,
+                          onPressed: () {
+                            disminuir();
+                          },
+                          icon: const Icon(
+                            Icons.arrow_drop_down,
+                            size: 15,
+                          )),
+                    )
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
+  }
+
+  void disminuir() {
+    if (pickerValue > 0) {
+      setState(() {
+        pickerValue--;
+        widget.textController.text = getTimeValue(pickerValue);
+      });
+    }
+  }
+
+  void aumentar() {
+    if (pickerValue < widget.max) {
+      setState(() {
+        pickerValue++;
+        widget.textController.text = getTimeValue(pickerValue);
+      });
+    }
   }
 }
